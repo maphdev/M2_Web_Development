bd212f7ae0c1deea1a00cc97b0010a631d9f6cd5
Manon Philippot <manon.philippot@gmail.com>
2018-09-28 09:58:09 +0200

    display json data

--- TD03_Single_Page_Application/my.js
+++ TD03_Single_Page_Application/my.js
@@ -3,9 +3,31 @@ $(document).ready(function () {
   const kidareasURL = 'http://odata.bordeaux.fr/v1/databordeaux/airejeux/?format=json&callback=?';
 
   function getData(URL){
-    $.getJSON(URL, function(result){
-       console.log(result);
+    deleteAllInnerElements('data-list');
+    $.getJSON(URL, function(result) {
+      console.log(result);
+
+       let nbElements = result['d'].length;
+
+       for (i = 0; i < nbElements; i++) {
+         console.log(result['d'][i].nom);
+         addListElement('data-list', result['d'][i].nom);
+       }
     });
+  }
+
+  function addListElement(parentId, string){
+    let parent = document.getElementById(parentId);
+    let li = document.createElement('li');
+    li.textContent = string;
+    parent.appendChild(li);
+  }
+
+  function deleteAllInnerElements(parentId){
+    let parent = document.getElementById(parentId);
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
   }
 
   $('#btn-toilets').click(function () {
